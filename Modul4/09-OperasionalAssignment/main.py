8cc643c9328abcecea24703450741158e9b34ccd
luudayat <luudayat@gmail.com>
2024-11-26 22:32:11 +0800

    Add files via upload

---
+++ Modul4/09-OperasionalAssignment/main.py
@@ -0,0 +1,27 @@
+# Operasional yang dapat dilakukan dengan penyingkatan
+# Operasio di tambah dengan assignment
+
+a = 5 # adalah assignment
+print("nilai a =" ,a)
+
+a += 5 # artinya adalah a = a + 1
+print("nilai a += 1, nilai a menjadi" ,a)
+
+a -= 5 # artinya adalah a = a - 1
+print("nilai a -= 1, nilai a menjadi" ,a)
+
+a *= 5 # artinya adalah a = a * 1
+print("nilai a *= 1, nilai a menjadi" ,a)
+
+a /= 5 # artinya adalah a = a / 1
+print("nilai a /= 1, nilai a menjadi" ,a)
+
+b = 10
+print("/nnilai b =" ,b)
+
+# Modulus dan floar division
+b %= 3
+print("nilai b %= 3, nilai b menjadi " ,b)
+
+b = 3
+print("nilai b %= 3, nilai b menjadi " ,b)
---
+++ Modul4/10-DataString/main.py
@@ -0,0 +1,24 @@
+data = "ini adalah string"
+print(data)
+print(type(data))
+
+# 1. cara membuat string
+
+'''
+1. Menggunakan singele quotes
+2. Menggunakan double quotes
+'''
+data = 'menggunakan singele quotes'
+print(data)
+
+data = 'menggunakan double quotes'
+print(data)
+
+print('"Hallo, apa kabar"')
+print("'Hallo, apa kabar'")
+print("ini adalah hari jum'at")
+
+#2. Menggunakn tanda /
+
+# membuat tanda ' menjadi string
+print('mari shalat jum/'at')
